a47b5506476f9d0e4dbb2eb24cd22da61f42eb65
Francois Lebel <francoislebel@gmail.com>
2013-04-21 21:25:00 -0400

    Include the number of available bikes and docks from the latest update.

--- bixi/api.py
+++ bixi/api.py
@@ -1,9 +1,15 @@
 from tastypie.resources import ModelResource
 
-from models import Station
+from models import Station, Update
 
 
 class StationResource(ModelResource):
+    def dehydrate(self, bundle):
+        update = Update.objects.filter(station__id=bundle.data['id']).latest()
+        bundle.data['nb_bikes'] = update.nb_bikes
+        bundle.data['nb_empty_docks'] = update.nb_empty_docks
+        return bundle
+
     class Meta:
         allowed_methods = ['get']
         queryset = Station.objects.all()
