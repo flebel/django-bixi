cb257504fba7f466db4cb89ac7e3497fe3254204
Francois Lebel <francoislebel@gmail.com>
2014-06-17 20:51:25 -0700

    Added Travis-CI support.

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='bixi',
+      version='0.3',
+      description='Django app for managing data from the Bixi public bicycle sharing system.',
+      author='Francois Lebel',
+      author_email='francoislebel@gmail.com',
+      url='https://github.com/flebel/django-bixi/',
+      packages=find_packages(),
+
+      test_suite='setuptest.setuptest.SetupTestSuite',
+      tests_require=(
+        'django-setuptest',
+        # Required by django-setuptools on Python 2.6
+        'argparse'
+      ),
+)
---
+++ test_settings.py
@@ -0,0 +1,11 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3'
+    }
+}
+
+INSTALLED_APPS = (
+    'south',
+
+    'bixi',
+)
