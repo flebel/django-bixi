8ccc922c191edd9407274d6c61c9de005c1ee515
Francois Lebel <francoislebel@gmail.com>
2014-07-30 18:58:54 -0700

    Added field descriptions where useful.

--- bixi/models.py
+++ bixi/models.py
@@ -13,12 +13,25 @@ class City(models.Model):
     PARSER_TYPES_VALUES = ('A', 'B', 'C',)
     PARSER_TYPES = [(val[1], val[0],) for val in enumerate(PARSER_TYPES_VALUES)]
 
-    code = models.SlugField(max_length=20)
+    code = models.SlugField(
+        help_text='Lowercase slug identifying the city.',
+        max_length=20
+    )
     name = models.CharField(max_length=200)
-    url = models.URLField()
-    parser_type = models.SmallIntegerField(choices=PARSER_TYPES, default=0)
-    active = models.BooleanField(default=True)
-    last_update = models.DateTimeField(null=True)
+    url = models.URLField(help_text='JSON or XML URL containing station updates. Both HTTP and HTTPS are supported.')
+    parser_type = models.SmallIntegerField(
+        choices=PARSER_TYPES,
+        default=0,
+        help_text='Parser to use for the given URL.'
+    )
+    active = models.BooleanField(
+        default=True,
+        help_text='Whether or not the city operates the bicycle sharing system.'
+    )
+    last_update = models.DateTimeField(
+        help_text='Date and time at which an update was recorded on the bicycle sharing system. Null if this information is unavailable.',
+        null=True
+    )
 
     objects = models.Manager()
     available = AvailableCityManager()
@@ -43,18 +56,24 @@ class AvailableStationManager(models.Manager):
 
 class Station(models.Model):
     city = models.ForeignKey(City)
-    public_id = models.IntegerField()
+    public_id = models.IntegerField(help_text='Unique identifier on the bicycle sharing system.')
     name = models.CharField(max_length=200)
-    terminal_name = models.CharField(max_length=50)
-    last_comm_with_server = models.DateTimeField(null=True)
+    terminal_name = models.CharField(
+        help_text='The general area where the station is located. A terminal name can be shared by multiple stations.',
+        max_length=50
+    )
+    last_comm_with_server = models.DateTimeField(
+        help_text='Date and time of the last communication with the bicycle sharing system.',
+        null=True
+    )
     lat = models.FloatField()
     long = models.FloatField()
-    installed = models.BooleanField()
-    locked = models.NullBooleanField()
+    installed = models.BooleanField(help_text='Whether or not this station is currently deployed.')
+    locked = models.NullBooleanField(help_text='Whether or not this station is accessible.')
     install_date = models.DateTimeField(null=True)
     removal_date = models.DateTimeField(null=True)
-    temporary = models.BooleanField()
-    public = models.NullBooleanField()
+    temporary = models.BooleanField(help_text='Whether or not this station is permanent.')
+    public = models.NullBooleanField(help_text='Whether or not this station is available for public use.')
 
     objects = models.Manager()
     available = AvailableStationManager()
@@ -97,10 +116,16 @@ class Station(models.Model):
 
 class Update(models.Model):
     station = models.ForeignKey(Station)
-    nb_bikes = models.IntegerField()
-    nb_empty_docks = models.IntegerField()
-    latest_update_time = models.DateTimeField(null=True)
-    raw_data = models.TextField(null=True)
+    nb_bikes = models.IntegerField(help_text='Number of bicycles docked at that moment.')
+    nb_empty_docks = models.IntegerField(help_text='Number of empty docks at that moment.')
+    latest_update_time = models.DateTimeField(
+        help_text='Date and time this update was recorded on the bicycle sharing system. Null if this information is unavailable.',
+        null=True
+    )
+    raw_data = models.TextField(
+        help_text="Unparsed update data acquired from the city's bicycle sharing system.",
+        null=True
+    )
 
     class Meta:
         get_latest_by = 'pk'
