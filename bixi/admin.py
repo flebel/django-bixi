d5ebc665fa2382d5ccccfaa8510a552a00364b13
Francois Lebel <francoislebel@gmail.com>
2014-08-15 23:11:23 -0700

    Updated fields for admin panel.

--- bixi/admin.py
+++ bixi/admin.py
@@ -8,18 +8,18 @@ from models import City, Station, Update
 
 class CityAdmin(admin.ModelAdmin):
     fields = ('name', 'code', 'url', 'active',)
-    list_display = ('name', 'code', 'url', 'last_update', 'active',)
+    list_display = ('name', 'code', 'url', 'last_recorded_update', 'active',)
     search_fields = ('name', 'code', 'active',)
 admin.site.register(City, CityAdmin)
 
 
 class StationAdmin(admin.ModelAdmin):
-    list_display = ('city', 'name', 'last_comm_with_server', 'installed',
+    list_display = ('city', 'name', 'last_recorded_communication', 'installed',
         'locked', 'temporary', 'public')
     list_filter = ('city', 'installed', 'locked', 'temporary', 'public')
-    readonly_fields = ('city', 'public_id', 'name', 'terminal_name',
-        'last_comm_with_server', 'lat', 'long', 'installed', 'locked',
-        'install_date', 'removal_date', 'temporary', 'public',)
+    readonly_fields = ('city', 'public_id', 'name', 'vicinity',
+        'last_recorded_communication', 'latitude', 'longitude', 'installed',
+        'locked', 'installation_date', 'removal_date', 'temporary', 'public',)
     search_fields = ('name',)
 
     def has_add_permission(self, request):
@@ -51,11 +51,11 @@ class LatestUpdatesListFilter(BooleanFieldListFilter):
 
 
 class UpdateAdmin(admin.ModelAdmin):
-    list_display = ('station', 'nb_bikes', 'nb_empty_docks',
+    list_display = ('station', 'bikes', 'empty_docks',
         'latest_update_time',)
     list_filter = (('latest_update_time', LatestUpdatesListFilter,),
         'station__city__name',)
-    readonly_fields = ('station', 'nb_bikes', 'nb_empty_docks',
+    readonly_fields = ('station', 'bikes', 'empty_docks',
         'latest_update_time', 'raw_data',)
     search_fields = ('station__name',)
 
