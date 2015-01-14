234b87fd6f55dee79623e89bb50db356a5296b39
Francois Lebel <francoislebel@gmail.com>
2015-01-14 08:15:07 -0800

    Bumped version to 1.1.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bixi',
-      version='1.0',
+      version='1.1',
       description='Django app for managing data from the Bixi public bicycle sharing system.',
       author='Francois Lebel',
       author_email='francoislebel@gmail.com',
